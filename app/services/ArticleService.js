921885c0bbb7a618ebbeb7f3bf140a2a2681bfa5
Brandon Martinez <me@brandonmartinez.com>
2017-01-11 16:11:52 -0500

    Continuing down the path of implementing a service/class based structure.

--- app/services/ArticleService.js
+++ app/services/ArticleService.js
@@ -9,5 +9,23 @@ function ArticleService(repository) {
 
 var articleServiceParent = BaseService.extendService(ArticleService); // jshint ignore:line
 
+ArticleService.prototype.findAll = function () {
+    return articleServiceParent.findAll.call(this).then(function (results) {
+        // Stubbing in data since we don't have a database yet
+        if (!results || results.length === 0) {
+            results = [{
+                title: 'Something',
+                text: 'More something',
+                url: 'my-new-article'
+            }, {
+                title: 'Something 2',
+                text: 'More something something',
+                url: 'my-new-article-again'
+            }];
+        }
+
+        return results;
+    });
+};
 
 module.exports = ArticleService;
--- app/services/BaseService.js
+++ app/services/BaseService.js
@@ -1,18 +1,19 @@
 'use strict';
 
 function BaseService(repository) {
-  this.repository = repository;
+    this.repository = repository;
 }
 
 BaseService.prototype.findAll = function () {
-  return this.repository.findAll();
+    return this.repository.findAll();
 };
 
 BaseService.extendService = function (destination) {
-  // Based on: http://oli.me.uk/2013/06/01/prototypical-inheritance-done-right/
-  destination.prototype = Object.create(BaseService.prototype);
-  destination.prototype.constructor = destination;
-  return BaseService.prototype;
+    // Based on: http://oli.me.uk/2013/06/01/prototypical-inheritance-done-right/
+    destination.prototype = Object.create(BaseService.prototype);
+    destination.prototype.constructor = destination;
+    // TODO: can we assign the base to a "parent" property instead of relying on the return?
+    return BaseService.prototype;
 };
 
 module.exports = BaseService;
